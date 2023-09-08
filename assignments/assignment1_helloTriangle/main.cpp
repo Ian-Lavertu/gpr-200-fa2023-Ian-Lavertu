7a2130625e4e61255e84c708c7186e8c4ce0f796
Ian Lavertu <ian.lavertu@mymail.champlain.edu>
2023-09-08 11:33:00 -0400

    assignment completed until fragment shaders.

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,6 +8,29 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+float vertices[9] = {
+	//x   //y   //z
+	-0.5, -0.5, 0.0, // bottom left
+	 0.5, -0.5, 0.0, // bottom right
+	 0.0,  0.5, 0.0  // top center
+};
+
+const char* vertexShaderSource = R"(
+	#version 450
+	layout(location = 0) in vec3 vPos;
+	void main(){
+		gl_Postion = vec4(vpos,1.0);
+	}
+)";
+
+const char* fragmentShaderSource = R"(
+	#version 450
+	out vec4 FragColor;
+	void main(){
+		FragColor = vec4(1.0);
+	}
+)";
+
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
@@ -27,10 +50,45 @@ int main() {
 		return 1;
 	}
 
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//Define a new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for +send vertex data to GPU
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//define position attribute (3 floats)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	//Create a new vertex shader object
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(vertexShader);
+
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile shader : %s", infoLog);
+	}
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Draw calls
+		glBindVertexArray(vao);
+
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
