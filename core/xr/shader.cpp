4a9f3106398e7191edeb0e3b7816594f84404476
Ian Lavertu <ian.lavertu@mymail.champlain.edu>
2023-09-12 12:40:13 -0400

    Assignment 2 added Started own library (xr)

---
+++ core/xr/shader.cpp
@@ -0,0 +1,11 @@
+#include "shader.h"
+namespace xr {
+	std::string loadShaderSourceFromFile(const std::string& filePath) {
+		std::ifstream fstream(filePath);
+		if (!stream.is_open()) {
+			printf("Failed to load file %s");
+
+		}
+	}
+
+}
---
+++ core/xr/shader.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <sstream>
+#include <fstream>
+
+namespace xr {
+	std::string loadShaderSourceFromFile(const std::string& filePath);
+}
